4dbd1217e1a180574d5680003a2fb56ed56ddbfa
phartex <fateruvictor96@gmail.com>
2024-08-08 19:34:01 +0100

    update

--- src/app/pages/economic/economic.component.ts
+++ src/app/pages/economic/economic.component.ts
@@ -8,19 +8,148 @@ import { Component, OnInit } from '@angular/core';
 export class EconomicComponent implements OnInit {
   cards: any[] = [
     {
-      title: 'Card Title 1',
-      description: 'This is the description for card 1.',
+      title: 'How to thrive in an Economic Slowdown',
+      description:`With about 17.4 million enterprises, it is no denial that SMEs account forms approximately 50% and 90% of
+       both industrial jobs and the manufacturing sector respectively.`,
       imageUrl: 'assets/money.png'
     },
     {
-      title: 'Card Title 2',
-      description: 'This is the description for card 2.',
-      imageUrl: 'assets/money.png' 
+      title: 'How to thrive in an Economic Slowdown',
+      description:`With about 17.4 million enterprises, it is no denial that SMEs account forms approximately 50% and 90% of
+       both industrial jobs and the manufacturing sector respectively.`,
+      imageUrl: 'assets/money.png'
     },
+    {
+      title: 'How to thrive in an Economic Slowdown',
+      description:`With about 17.4 million enterprises, it is no denial that SMEs account forms approximately 50% and 90% of
+       both industrial jobs and the manufacturing sector respectively.`,
+      imageUrl: 'assets/money.png'
+    },
+    {
+      title: 'How to thrive in an Economic Slowdown',
+      description:`With about 17.4 million enterprises, it is no denial that SMEs account forms approximately 50% and 90% of
+       both industrial jobs and the manufacturing sector respectively.`,
+      imageUrl: 'assets/money.png'
+    },
+    {
+      title: 'How to thrive in an Economic Slowdown',
+      description:`With about 17.4 million enterprises, it is no denial that SMEs account forms approximately 50% and 90% of
+       both industrial jobs and the manufacturing sector respectively.`,
+      imageUrl: 'assets/money.png'
+    },
+    {
+      title: 'How to thrive in an Economic Slowdown',
+      description:`With about 17.4 million enterprises, it is no denial that SMEs account forms approximately 50% and 90% of
+       both industrial jobs and the manufacturing sector respectively.`,
+      imageUrl: 'assets/money.png'
+    },
+    {
+      title: 'How to thrive in an Economic Slowdown',
+      description:`With about 17.4 million enterprises, it is no denial that SMEs account forms approximately 50% and 90% of
+       both industrial jobs and the manufacturing sector respectively.`,
+      imageUrl: 'assets/money.png'
+    },
+    {
+      title: 'How to thrive in an Economic Slowdown',
+      description:`With about 17.4 million enterprises, it is no denial that SMEs account forms approximately 50% and 90% of
+       both industrial jobs and the manufacturing sector respectively.`,
+      imageUrl: 'assets/money.png'
+    },
+    {
+      title: 'How to thrive in an Economic Slowdown',
+      description:`With about 17.4 million enterprises, it is no denial that SMEs account forms approximately 50% and 90% of
+       both industrial jobs and the manufacturing sector respectively.`,
+      imageUrl: 'assets/money.png'
+    },
+    {
+      title: 'How to thrive in an Economic Slowdown',
+      description:`With about 17.4 million enterprises, it is no denial that SMEs account forms approximately 50% and 90% of
+       both industrial jobs and the manufacturing sector respectively.`,
+      imageUrl: 'assets/money.png'
+    },
+    {
+      title: 'How to thrive in an Economic Slowdown',
+      description:`With about 17.4 million enterprises, it is no denial that SMEs account forms approximately 50% and 90% of
+       both industrial jobs and the manufacturing sector respectively.`,
+      imageUrl: 'assets/money.png'
+    },
+    {
+      title: 'How to thrive in an Economic Slowdown',
+      description:`With about 17.4 million enterprises, it is no denial that SMEs account forms approximately 50% and 90% of
+       both industrial jobs and the manufacturing sector respectively.`,
+      imageUrl: 'assets/money.png'
+    },
+    {
+      title: 'How to thrive in an Economic Slowdown',
+      description:`With about 17.4 million enterprises, it is no denial that SMEs account forms approximately 50% and 90% of
+       both industrial jobs and the manufacturing sector respectively.`,
+      imageUrl: 'assets/money.png'
+    },
+    {
+      title: 'How to thrive in an Economic Slowdown',
+      description:`With about 17.4 million enterprises, it is no denial that SMEs account forms approximately 50% and 90% of
+       both industrial jobs and the manufacturing sector respectively.`,
+      imageUrl: 'assets/money.png'
+    },
+    {
+      title: 'How to thrive in an Economic Slowdown',
+      description:`With about 17.4 million enterprises, it is no denial that SMEs account forms approximately 50% and 90% of
+       both industrial jobs and the manufacturing sector respectively.`,
+      imageUrl: 'assets/money.png'
+    },
+    {
+      title: 'How to thrive in an Economic Slowdown',
+      description:`With about 17.4 million enterprises, it is no denial that SMEs account forms approximately 50% and 90% of
+       both industrial jobs and the manufacturing sector respectively.`,
+      imageUrl: 'assets/money.png'
+    },
+    {
+      title: 'How to thrive in an Economic Slowdown',
+      description:`With about 17.4 million enterprises, it is no denial that SMEs account forms approximately 50% and 90% of
+       both industrial jobs and the manufacturing sector respectively.`,
+      imageUrl: 'assets/money.png'
+    },
+    {
+      title: 'How to thrive in an Economic Slowdown',
+      description:`With about 17.4 million enterprises, it is no denial that SMEs account forms approximately 50% and 90% of
+       both industrial jobs and the manufacturing sector respectively.`,
+      imageUrl: 'assets/money.png'
+    },
+    {
+      title: 'How to thrive in an Economic Slowdown',
+      description:`With about 17.4 million enterprises, it is no denial that SMEs account forms approximately 50% and 90% of
+       both industrial jobs and the manufacturing sector respectively.`,
+      imageUrl: 'assets/money.png'
+    },
+    {
+      title: 'How to thrive in an Economic Slowdown',
+      description:`With about 17.4 million enterprises, it is no denial that SMEs account forms approximately 50% and 90% of
+       both industrial jobs and the manufacturing sector respectively.`,
+      imageUrl: 'assets/money.png'
+    },
+   
   ];
+
+  currentPage: number = 1;
+  itemsPerPage: number = 6;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  getPaginatedCards(): any[] {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    return this.cards.slice(startIndex, endIndex);
+  }
+
+  setPage(page: number): void {
+    if (page > 0 && page <= this.totalPages()) {
+      this.currentPage = page;
+    }
+  }
+
+  totalPages(): number {
+    return Math.ceil(this.cards.length / this.itemsPerPage);
+  }
 }
