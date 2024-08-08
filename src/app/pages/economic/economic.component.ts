f0413ca7aacaefb835a43b20e675d8b9b1c1cda8
phartex <fateruvictor96@gmail.com>
2024-08-08 18:44:51 +0100

    update

--- src/app/pages/economic/economic.component.ts
+++ src/app/pages/economic/economic.component.ts
@@ -6,7 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./economic.component.scss']
 })
 export class EconomicComponent implements OnInit {
-
+  cards: any[] = [
+    {
+      title: 'Card Title 1',
+      description: 'This is the description for card 1.',
+      imageUrl: 'assets/money.png'
+    },
+    {
+      title: 'Card Title 2',
+      description: 'This is the description for card 2.',
+      imageUrl: 'assets/money.png' 
+    },
+  ];
   constructor() { }
 
   ngOnInit(): void {
