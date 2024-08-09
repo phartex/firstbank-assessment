71a4c38ea3b5699c51331ea37bcfdfbcd388ffb0
phartex <fateruvictor96@gmail.com>
2024-08-09 08:25:45 +0100

    did changing of background n homepage

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -6,10 +6,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
+  // background: url(/assets/fbn-personal2.png);
+  backgroundImages = [
+    '/assets/fbn-personal2.png',
+    '/assets/fbn-personal2.png',
+    ''
+  ];
+
+  currentIndex = 0;
+  activeLine = 1;
+
+  touchStartX = 0;
+  touchEndX = 0;
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onTouchStart(event: TouchEvent) {
+    this.touchStartX = event.changedTouches[0].screenX;
+  }
+
+  onTouchEnd(event: TouchEvent) {
+    this.touchEndX = event.changedTouches[0].screenX;
+    this.handleSwipe();
+  }
+
+  handleSwipe() {
+    if (this.touchEndX < this.touchStartX) {
+      this.nextImage();
+    }
+
+    if (this.touchEndX > this.touchStartX) {
+      this.prevImage();
+    }
+  }
+
+  nextImage() {
+    if (this.currentIndex < this.backgroundImages.length - 1) {
+      this.currentIndex++;
+      this.updateActiveLine();
+    }
+  }
+
+  prevImage() {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+      this.updateActiveLine();
+    }
+  }
+
+  changeBackground(index: number) {
+    this.currentIndex = index;
+    this.updateActiveLine();
+  }
+
+  updateActiveLine() {
+    this.activeLine = (this.currentIndex % 3) + 1;
   }
 
 }
